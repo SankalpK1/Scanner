6ff3561f9a351efb8e7c641171edb23e0a4db320
SankalpK1 <32941312+SankalpK1@users.noreply.github.com>
2019-05-22 07:25:23 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
     cameraTrigger = document.querySelector("#camera--trigger")
+    switchtheTrigger = document.querySelector("#switch--trigger")
 // Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
